8241ac7b3214da97ad720f8ed892231e3264379a
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-07-04 13:15:28 +0530

    Time: 4 ms (71.65%), Space: 12.1 MB (19.88%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        return;
+        int ind=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]) {
+                ind=i;
+                break;
+            }
+        }
+        // cout<<ind<<endl;
+        if(ind<0){ reverse(nums.begin(),nums.end());}
+        else {for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
+            
+
+        }
+              reverse(nums.begin()+ind+1,nums.end());}
         
     }
 };
