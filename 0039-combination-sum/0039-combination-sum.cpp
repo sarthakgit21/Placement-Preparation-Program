3845a2e18d5cf6bbe7e6995fdffe4ac6b02bf5b0
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-07-13 23:18:07 +0530

    Time: 3 ms (93.17%), Space: 10.8 MB (65.69%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -6,10 +6,14 @@ public:
             return;
         }
         for(int i=ind;i<candidates.size();i++){
+            // if()
             if(candidates[i]<=target){
                 arr.push_back(candidates[i]);
                 numFun(candidates,ans,target-candidates[i],i,arr);
                 arr.pop_back();
+            }
+            else{
+                break;
             }
         }
         
@@ -18,6 +22,7 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> arr;
+        sort(begin(candidates),end(candidates));
         numFun(candidates,ans,target,0,arr);
         return ans;
     }
