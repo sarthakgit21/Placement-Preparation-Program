686c8a02477cfc37695686b8c0cc9bc88357bbd6
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-07-26 23:03:45 +0530

    Create maxDepth.cpp

---
+++ DAY 18/maxDepth.cpp
@@ -0,0 +1,7 @@
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int l=maxDepth(root->left);
+        int r=maxDepth(root->right);
+        // int r=
+        return max(l,r)+1;
+    }
